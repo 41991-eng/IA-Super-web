bde95e32f2015fa70f70805b7fd7a47d0822a08f
41991-eng <41991@raphaeldisanto.com.br>
2026-04-16 08:04:47 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import './App.css'
 
 const features = [
-  { title: "Nacional Basket Association", desc: "Dados armazenados como documentos BSON flexíveis, sem esquema fixo." },
-  {  title: "Alta Performance esportiva", desc: "Índices poderosos e consultas rápidas mesmo em grande volume de dados." },
-  {  title: "Regras não escritas", desc: "Sharding horizontal nativo para crescer conforme a demanda." },
-  {  title: "Replica Set", desc: "Alta disponibilidade com replicação automática e failover." },
+  { title: "NBA", desc: "Nacional Basket Association." },
+  {  title: "Alta Performance esportiva", desc: "A liga obriga os atletas a ficarem em boa condição quase o ano tode sob pouco tempo de descanço, ja que são 82, apenas em temporada regular" },
+  {  title: "Regras não escritas", desc: "Mesmo que não estejam no livro oficial da NBA, quebrar essas regras pode gerar brigas, multas e uma péssima reputação entre os colegas." },
+  {  title: "Conferências e Divisões", desc: "A NBA divide suas 30 franquias(West Coast & East Coast) para facilitar a logística de viagens e fomentar rivalidades regionais." },
 ];
 
 export default function App() {
   return (
-    <>
-      
-    </>
-  )
+     <div className="page">
+      <header className="hero">
+        <a href='https://www.nba.com/' className="hero-title">
+          <span className="super-nba">NBA</span>
+        </a>
+        <p className="hero-sub">
+          A liga de basquete mais disputada do planeta.
+        </p>
+      </header>
+ 
+      <section className="features">
+        {features.map((f) => (
+          <div className="card" key={f.title}>
+            <h3>{f.title}</h3>
+            <p>{f.desc}</p>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
 }
-
+ 
